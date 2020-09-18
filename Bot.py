270576aacd70bf6267a389e6bd7ed07caf1d7958
Tatu Holkko <tatu.holkko@gmail.com>
2020-09-19 02:43:53 +0300

    add get_distribution and config limit functions

--- Bot.py
+++ Bot.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 HIT = 2
 MISS = 1
@@ -13,7 +14,8 @@ shots:  A board matrix where 1 indicates a miss,
         2 indicates a hit and 0 can be either
 
 ships:  A board matrix where 1 indicates a ship and
-        0 is no ship
+        0 is no ship. This represents the current guess
+        where the ships could be
 
 '''
 
@@ -57,8 +59,9 @@ def can_place(ships, shots, x, y, length, orientation):
             return False
         if ships[y_][x_] == SHIP:
             return False
+    return True
 
-def place_ship(ships, x, y, length, orientation)
+def place_ship(ships, x, y, length, orientation):
     """
     Place a ship into the ships array. This function does not
     check if the placement is legal or not.
@@ -97,3 +100,79 @@ def add_configuration(distribution, ships):
     for y in range(len(distribution)):
         for x in range(len(distribution[y])):
             distribution[y][x] += ships[y][x]
+
+def get_distribution(ships, shots, ship_sizes):
+    """
+    Returns a numpy array that contains in each square the number of times
+    the square was occupied by a ship out of all possible configurations
+
+    Args:
+        ships: ship matrix
+        shots: shot matrix
+        ship_sizes (Array): list of ship sizes that should be placed
+
+    Returns:
+        np.array: 
+            a numpy array that contains in each square the number of times
+            the square was occupied by a ship out of all possible 
+            configurations
+    """
+    board_width = len(ships)
+    board_height = len(ships[0])
+    distr = np.zeros([board_height, board_width])
+    ship_size = ship_sizes[-1]
+    for orientation in [HORIZONTAL, VERTICAL]:
+        
+        #in the direction parallel to the ship the amount
+        #of possible locations is restricted by the ships length
+        y_max = board_height
+        x_max = board_width
+        if orientation == HORIZONTAL:
+            x_max = board_width - ship_size + 1
+        else:
+            y_max = board_height - ship_size + 1
+
+        for y in range(y_max):
+            for x in range(x_max):
+                
+                if not can_place(ships, shots, x, y, ship_size, orientation):
+                    continue
+                
+                new_ships = np.copy(ships)
+                place_ship(new_ships, x, y, ship_size, orientation)
+                new_ship_sizes = ship_sizes[:-1]
+
+                if len(new_ship_sizes) == 0:
+                    #all ships placed
+                    if board_is_possible(new_ships, shots):
+                        #the board is possible
+                        #add one to each square occupied by a ship
+                        distr = distr + new_ships
+                        print(new_ships)
+                else:
+                    distr += get_distribution(new_ships, shots, new_ship_sizes)
+        
+    return distr
+
+def config_limit(width, height, ship_sizes):
+    """
+    Return the amount of different configurations
+    if the ships are allowed to overlap
+
+    Args:
+        width (int): board width
+        height (int): board height
+        ship_sizes (Array): List of ship sizes to place
+    """
+    total = 1
+    for size in ship_sizes:
+        total *= (width - size + 1) * height + (height - size + 1) * width
+    return total
+
+
+ships = np.zeros([10,10])
+shots = np.zeros([10,10])
+shots[5][5] = MISS
+ship_sizes = [2,3,4]
+print("Limit of configurations with collisions:", config_limit(10,10, ship_sizes))
+print(get_distribution(ships, shots, ship_sizes))
