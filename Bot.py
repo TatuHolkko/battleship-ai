6caced284ccaa1bc019f5d43ec0853505a2d0251
Tatu Holkko <tatu.holkko@gmail.com>
2020-09-18 20:55:59 +0300

    add place_ship function

--- Bot.py
+++ Bot.py
@@ -9,10 +9,10 @@ VERTICAL = 2
 '''
 explanations of the grid arguments "shots" and "ships"
 
-shots:  A board matrix where 1 indicates a miss, 
+shots:  A board matrix where 1 indicates a miss,
         2 indicates a hit and 0 can be either
 
-ships:  A board matrix where 1 indicates a ship and 
+ships:  A board matrix where 1 indicates a ship and
         0 is no ship
 
 '''
@@ -39,10 +39,10 @@ def can_place(ships, shots, x, y, length, orientation):
     ships or missed shot squares
 
     Args:
-        shots: shot matrix
         ships: ship matrix
-        x (int): x coordinate of one end of the ship
-        y (int): y coordinate of one end of the ship
+        shots: shot matrix
+        x (int): x coordinate of top left end of the ship
+        y (int): y coordinate of top left end of the ship
         length (int): length of the ship
         orientation (int): HORIZONTAL (1) or VERTICAL (2)
     """
@@ -57,3 +57,25 @@ def can_place(ships, shots, x, y, length, orientation):
             return False
         if ships[y_][x_] == SHIP:
             return False
+
+def place_ship(ships, x, y, length, orientation)
+    """
+    Place a ship into the ships array. This function does not
+    check if the placement is legal or not.
+
+    Args:
+        ships: ship matrix
+        x (int): x coordinate of top left end of the ship
+        y (int): y coordinate of top left end of the ship
+        length (int): length of the ship
+        orientation (int): HORIZONTAL (1) or VERTICAL (2)
+    """
+    for i in range(length):
+        x_ = x
+        y_ = y
+        if orientation == HORIZONTAL:
+            x_ = x + i
+        else:
+            y_ = y + i
+
+        ships[y_][x_] = SHIP
